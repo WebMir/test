5aac0126f331631688046b1d830b5308c15ef30e
albert <73446485+WebMir@users.noreply.github.com>
2021-01-15 22:28:11 +0300

    Сделал моб меню

--- js/script.js
+++ js/script.js
@@ -10,23 +10,47 @@ let icClose1 = icClose.style.display;
 // icClose.setAttribute('style', 'display: none')
 
 
-function clickMenu(zzz) {
-    let i = 'block';
+// function clickMenu(zzz) {
+//     let i = 'block';
 
-    if (i == icMenu1){
+//     if (i == icMenu1){
+//         icMenu.setAttribute('style', 'display: none')
+//         icClose.setAttribute('style', 'display: block')
+
+//        alert('1111111')
+//     }
+// }
+
+// function clickClose(CCC) {
+//     let i = 'block';
+
+//     if (i == icMenu1){
+//         icClose.setAttribute('style', 'display: none')
+//         icMenu.setAttribute('style', 'display: block')
+//         alert('000000000')
+//     }
+// }
+
+
+
+
+
+
+
+
+svgMenu.setAttribute('data-theme', 'light')
+
+svgMenu.addEventListener('click', () => {
+
+    if (svgMenu.getAttribute('data-theme') === 'light') {
+        svgMenu.setAttribute('data-theme', 'dark')
         icMenu.setAttribute('style', 'display: none')
         icClose.setAttribute('style', 'display: block')
-
-       alert('1111111')
-    }
-}
-
-function clickClose(CCC) {
-    let i = 'block';
-
-    if (i == icMenu1){
+    } else {
+        svgMenu.setAttribute('data-theme', 'light')
         icClose.setAttribute('style', 'display: none')
         icMenu.setAttribute('style', 'display: block')
-        alert('000000000')
     }
-}
+})
+
+
